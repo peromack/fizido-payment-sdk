0d0fd1b90a2bd09304e297ec4e36d6dd09fc2e43
Amos Godwin <Amosgodwin500@gmail.com>
2021-07-15 20:54:06 +0100

    changed minsdk version

--- lib/empressa_pos.dart
+++ lib/empressa_pos.dart
@@ -5,13 +5,8 @@ import 'package:empressa_pos/card_details.dart';
 import 'package:flutter/services.dart';
 
 class EmpressaPos {
-  static const MethodChannel _channel =
-      const MethodChannel('empressa_pos');
+  static const MethodChannel _channel = const MethodChannel('empressa_pos');
 
-  static Future<Map<String,String>> get platformVersion async {
-    final Map<String,String> version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
 
   static Future<CardDetails> search() async {
     CardDetails cardDetails ;
@@ -24,8 +19,8 @@ class EmpressaPos {
         var track2Data = cardDetails.the57 ;
         var strTrack2 = track2Data.split("F")[0] ;
         var pan = strTrack2.split('D')[0] ;
-        var expiry = strTrack2.split("D")[1].substring(0,4);
-        var src = strTrack2.split("D")[1].substring(4,7);
+        var expiry = strTrack2.split('D')[1].substring(0,4);
+        var src = strTrack2.split('D')[1].substring(4,7);
         cardDetails.strTrack2 = strTrack2 ;
         cardDetails.pan = pan ;
         cardDetails.expiry = expiry ;
